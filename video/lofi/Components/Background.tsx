360462bf857229cb27659b2e1189b278f84ab3bd
Luc Van Minh <luk.mink@gmail.com>
2024-10-17 22:26:33 +0700

    adding template

--- video/lofi/Components/Background.tsx
+++ video/lofi/Components/Background.tsx
@@ -3,7 +3,6 @@ import {Img, staticFile} from 'remotion';
 export const Background: React.FC<{backgroundPath: string}> = ({
 	backgroundPath: backgroundPath,
 }) => {
-	console.log('backgroundPath', backgroundPath);
 	return (
 		<div className="w-full h-full brightness-90">
 			<Img
